ed84be96f54dd07767a46ff5c62bd9151fd50f83
MrIceFox <extremetsa@gmail.com>
2018-04-16 19:33:26 +0800

    modify target register api

--- archmage-annotation/src/main/java/com/mricefox/archmage/annotation/Target.java
+++ archmage-annotation/src/main/java/com/mricefox/archmage/annotation/Target.java
@@ -1,17 +1,17 @@
 package com.mricefox.archmage.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
  * <p>Author:MrIcefox
  * <p>Email:extremetsa@gmail.com
- * <p>Description:
- * <p>Date:2018/1/24
+ * <p>Description:Mark target
+ * <p>Date:2018/4/7
  */
 @Retention(RetentionPolicy.CLASS)
-public @interface TargetMapping {
+@java.lang.annotation.Target(ElementType.TYPE)
+public @interface Target {
     String path();
-
-    Class<?> target();
 }
